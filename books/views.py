091f19c8ce3ebaf65eed044b2f61cfc8d28e264c
ahmednassar855 <ahmed.nassar855@yahoo.com>
2023-10-16 20:09:04 +0300

    ch14

--- books/views.py
+++ books/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin , PermissionRequiredMixin# new
 from django.views.generic import ListView , DetailView
 
 from .models import Book
 # Create your views here.
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin ,ListView):  # new
     model = Book
     context_object_name = "book_list"
     template_name = "books/book_list.html"
+    login_url = "account_login"   # new
 
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin,DetailView):
     model = Book
     context_object_name = "book_detail"
     template_name = "books/book_detail.html"
+    login_url = "account_login" # new
+    permission_required = "books.special_status"
